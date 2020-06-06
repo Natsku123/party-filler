2bf37dbb028ee0ca48f0ce7b905684bf0b037e51
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-06-06 15:54:46 +0300

    Modify login url

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const handleDiscordLogin = () => {
   const params = [
-    'client_id=SECRET',
-    'scope=identify',
-    'redirect_uri=coming_soon'
+    'client_id=718047907617439804',
+    'redirect_uir=http%3A%2F%2Fapi.party.hellshade.fi%2Foauth2%2Fcallback',
+    'response_type=code',
+    'scope=identify%20guilds'
   ].join('&')
 
   window.location.assign(`https://discord.com/api/oauth2/authorize?${params}`)
