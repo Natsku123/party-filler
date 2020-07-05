6771ee3cc743c26a6677f5d7e6a0cb13d5938da3
Elias Lehtinen <elias.lehtinen01@gmail.com>
2020-07-05 15:16:53 +0300

    Add information to party page are you leader and member

--- frontend/src/components/Party.js
+++ frontend/src/components/Party.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 
 import partyService from '../services/parties'
+import userService from '../services/users'
 
 const Party = () => {
   const id = useParams().id
   const [ party, setParty ] = useState(null)
+  const [ user, setUser ] = useState(null)
 
   useEffect(() => {
     partyService
@@ -13,13 +15,36 @@ const Party = () => {
       .then(res => setParty(res))
   }, [id])
 
+  useEffect(() => {
+    userService
+      .getUser()
+      .then(res => setUser(res))
+  }, [])
+
   if (!party) {
     return <div>loading...</div>
   }
 
+  const isMember = user && party.members
+    .map((member) => member.id)
+    .includes(user.id)
+
   return (
     <div>
       <h1>Party</h1>
+      { !user ?
+          <p>Et ole kirjautunut</p> :
+          <div>
+            { user.id === party.leaderId ?
+                <p>Olet johtaja</p> :
+                <p>Et ole johtaja</p>
+            }
+            { isMember ?
+                <p>Olet mukana</p> :
+                <p>Et ole mukana</p>
+            }
+          </div>
+      }
       <h2>Information</h2>
       <p>Party Id: {party.id}</p>
       <p>Title: {party.title}</p>
@@ -36,7 +61,7 @@ const Party = () => {
       */}
       <h2>Members</h2>
       <ol>
-        {party.members.map(member => <li>member.name)</li>)}
+        {party.members.map(member => <li key={member}>{member.name}</li>)}
       </ol>
     </div>
   )
