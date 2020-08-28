14bfbde14fcd04c6befb7dc227d06bac11e85289
Max Mecklin <max@meckl.in>
2020-08-29 00:12:31 +0300

    Update upgrade.sh

--- upgrade.sh
+++ upgrade.sh
@@ -4,5 +4,5 @@ echo "Warning! Confirm that you run this in the same directory!"
 
 # TODO add update download
 
-docker exec partyfiller_backend_1 flask db migrate -m "Update migration."
-docker exec partyfiller_backend_1 flask db upgrade
+docker exec partyfiller_backend_1 alembic revision --autogenerate -m "Upgrade."
+docker exec partyfiller_backend_1 alembic upgrade head
