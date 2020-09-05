0c908ef7f702faa7319e08eddea5427699472853
Max Mecklin <max@meckl.in>
2020-09-05 09:13:38 +0300

    Change secret key to fetch from env

--- backend/config/__init__.py
+++ backend/config/__init__.py
@@ -5,8 +5,8 @@ from pydantic import BaseSettings, AnyHttpUrl, validator
 
 
 class Settings(BaseSettings):
-    SERVER_NAME: str = os.environ.get("PartyFiller Backend")
-    SECRET_KEY: str = secrets.token_urlsafe(32)
+    SERVER_NAME: str = os.environ.get("SERVER_NAME", "PartyFiller Backend")
+    SECRET_KEY: str = os.environ.get("SECRET_KEY")
     API_HOSTNAME: AnyHttpUrl = os.environ.get("API_HOSTNAME", "http://localhost:8000")
     SITE_HOSTNAME: AnyHttpUrl = os.environ.get("SITE_HOSTNAME", "http://localhost:3000")
     BACKEND_CORS_ORIGINS: List[AnyHttpUrl] = [str(API_HOSTNAME), str(SITE_HOSTNAME)]
