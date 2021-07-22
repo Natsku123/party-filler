0c7baff06391f923ee4ae16ee1be91543ea84798
Max Mecklin <max@meckl.in>
2021-07-22 17:34:04 +0300

    Eslint ignores

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -6,7 +6,7 @@ module.exports = function(app) {
     createProxyMiddleware({
       target: 'http://localhost:8800',
       changeOrigin: true,
-      pathRewrite: {'^/api' : ''}
+      pathRewrite: { '^/api' : '' }
     })
   );
 };
