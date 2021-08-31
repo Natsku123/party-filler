7a77bca8ffc4b97e86c7bba49938d8885fcb861e
Max Mecklin <max@meckl.in>
2021-08-31 14:11:44 +0300

    Add support models for Game

--- backend/core/database/games.py
+++ backend/core/database/games.py
@@ -4,6 +4,45 @@ from sqlalchemy import Column, Integer, String
 
 
 class Game(SQLModel, table=True):
-    id: Optional[int] = Field(sa_column=Column(Integer, primary_key=True, unique=True))
-    name: str = Field(sa_column=Column(String(255), nullable=False))
-    default_max_players: int = Field(sa_column=Column(Integer))
+    id: Optional[int] = Field(
+        sa_column=Column(Integer, primary_key=True, unique=True),
+        description="ID of game",
+    )
+    name: str = Field(
+        sa_column=Column(String(255), nullable=False), description="Name of game"
+    )
+    default_max_players: int = Field(
+        sa_column=Column(Integer),
+        gt=0,
+        alias="defaultMaxPlayers",
+        description="Default number of maximum players for this game",
+    )
+
+
+class GameCreate(SQLModel):
+    name: str = Field(description="Name of game")
+    default_max_players: int = Field(
+        gt=0,
+        alias="defaultMaxPlayers",
+        description="Default number of maximum players for this game",
+    )
+
+
+class GameUpdate(SQLModel):
+    name: Optional[str] = Field(None, description="Name of game")
+    default_max_players: Optional[int] = Field(
+        None,
+        gt=0,
+        alias="defaultMaxPlayers",
+        description="Default number of maximum players for this game",
+    )
+
+
+class GameShort(SQLModel):
+    id: int = Field(..., description="ID of game")
+    name: str = Field(description="Name of game")
+    default_max_players: int = Field(
+        gt=0,
+        alias="defaultMaxPlayers",
+        description="Default number of maximum players for this game",
+    )
