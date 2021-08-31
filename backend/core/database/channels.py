2384adead67188b3f7a27854aab59eb0907e9910
Max Mecklin <max@meckl.in>
2021-08-31 14:02:56 +0300

    Add support models for Channel

--- backend/core/database/channels.py
+++ backend/core/database/channels.py
@@ -1,6 +1,6 @@
 from typing import Optional
-from sqlmodel import Field, SQLModel
-
+from sqlmodel import Field, SQLModel, Relationship
+from sqlalchemy.orm import relationship
 from sqlalchemy import (
     Column,
     ForeignKey,
@@ -10,9 +10,40 @@ from sqlalchemy import (
 
 
 class Channel(SQLModel, table=True):
-    id: Optional[int] = Field(sa_column=Column(Integer, primary_key=True, unique=True))
-    name: str = Field(sa_column=Column(String(255), nullable=False))
-    discord_id: str = Field(sa_column=Column(String(64), nullable=False, unique=True))
+    id: Optional[int] = Field(
+        sa_column=Column(Integer, primary_key=True, unique=True),
+        description="ID of channel",
+    )
+    name: str = Field(
+        sa_column=Column(String(255), nullable=False),
+        description="Name of channel from Discord",
+    )
+    discord_id: str = Field(
+        sa_column=Column(String(64), nullable=False, unique=True),
+        alias="discordId",
+        description="Discord ID of channel",
+    )
     server_id: int = Field(
-        sa_column=Column(Integer, ForeignKey("servers.id"), nullable=False)
+        sa_column=Column(Integer, ForeignKey("servers.id"), nullable=False),
+        alias="serverId",
+        description="ID of server associated with",
+    )
+    server: "Server" = Relationship(sa_relationship=relationship("Server"))
+
+
+class ChannelCreate(SQLModel):
+    name: Optional[str] = Field(None, description="Name of channel from Discord")
+    discord_id: str = Field(..., alias="discordId", description="Discord ID of channel")
+    server_id: Optional[int] = Field(
+        None, alias="serverId", description="ID of server associated with"
+    )
+
+
+class ChannelUpdate(SQLModel):
+    name: Optional[str] = Field(None, description="Name of channel from Discord")
+    discord_id: Optional[str] = Field(
+        None, alias="discordId", description="Discord ID of channel"
+    )
+    server_id: Optional[int] = Field(
+        None, alias="serverId", description="ID of server associated with"
     )
