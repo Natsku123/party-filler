2ce1a0c1247d7770fb1dc306a0a08eed93715741
Max Mecklin <max@meckl.in>
2021-09-01 10:57:26 +0300

    Add support models for Player

--- backend/core/database/players.py
+++ backend/core/database/players.py
@@ -11,11 +11,26 @@ from core.database import player_server_association
 
 
 class Player(SQLModel, table=True):
-    id: Optional[int] = Field(sa_column=Column(Integer, primary_key=True, unique=True))
-    discord_id: str = Field(sa_column=Column(String(64), nullable=False, unique=True))
-    name: str = Field(sa_column=Column(String(64), nullable=False))
-    discriminator: str = Field(sa_column=Column(String(4), nullable=False))
-    icon: str = Field(sa_column=Column(String(64)))
+    id: Optional[int] = Field(
+        sa_column=Column(Integer, primary_key=True, unique=True),
+        description="ID of player",
+    )
+    discord_id: str = Field(
+        sa_column=Column(String(64), nullable=False, unique=True),
+        alias="discordId",
+        description="ID on discord",
+    )
+    name: str = Field(
+        sa_column=Column(String(64), nullable=False),
+        description="Discord username of player",
+    )
+    discriminator: str = Field(
+        sa_column=Column(String(4), nullable=False),
+        description="Discord user discriminator",
+    )
+    icon: Optional[str] = Field(
+        sa_column=Column(String(64)), description="Discord icon hash"
+    )
 
     servers: List["Server"] = Relationship(
         sa_relationship=relationship(
@@ -31,3 +46,27 @@ class Player(SQLModel, table=True):
             "discriminator": self.discriminator,
             "icon": self.icon,
         }
+
+
+class PlayerCreate(SQLModel):
+    discord_id: str = Field(alias="discordId", description="ID on discord")
+    name: str = Field(description="Discord username of player")
+    discriminator: str = Field(description="Discord user discriminator")
+    icon: Optional[str] = Field(None, description="Discord icon hash")
+
+
+class PlayerUpdate(SQLModel):
+    discord_id: Optional[str] = Field(
+        None, alias="discordId", description="ID on discord"
+    )
+    name: Optional[str] = Field(None, description="Discord username of player")
+    discriminator: Optional[str] = Field(None, description="Discord user discriminator")
+    icon: Optional[str] = Field(None, description="Discord icon hash")
+
+
+class PlayerShort(SQLModel):
+    id: int = Field(description="ID of player")
+    discord_id: str = Field(alias="discordId", description="ID on discord")
+    name: str = Field(description="Discord username of player")
+    discriminator: str = Field(description="Discord user discriminator")
+    icon: Optional[str] = Field(None, description="Discord icon hash")
