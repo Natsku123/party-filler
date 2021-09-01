0e56e25e072714225dbc574e1d43426f01db276d
Max Mecklin <max@meckl.in>
2021-09-01 11:01:38 +0300

    Add support models for Role

--- backend/core/database/roles.py
+++ backend/core/database/roles.py
@@ -5,11 +5,57 @@ from sqlalchemy import Column, Integer, String, ForeignKey
 
 
 class Role(SQLModel, table=True):
-    id: Optional[int] = Field(sa_column=Column(Integer, primary_key=True, unique=True))
-    party_id: int = Field(sa_column=Column(Integer, ForeignKey("parties.id")))
-    name: str = Field(sa_column=Column(String(64)))
-    max_players: int = Field(sa_column=Column(Integer))
+    id: Optional[int] = Field(
+        sa_column=Column(Integer, primary_key=True, unique=True),
+        description="ID of role",
+    )
+    party_id: Optional[int] = Field(
+        sa_column=Column(Integer, ForeignKey("parties.id")),
+        gt=0,
+        alias="partyId",
+        description="ID of party",
+    )
+    name: Optional[str] = Field(
+        sa_column=Column(String(64)), description="Name of role"
+    )
+    max_players: Optional[int] = Field(
+        sa_column=Column(Integer),
+        gt=0,
+        alias="maxPlayers",
+        description="Maximum number of players of role",
+    )
 
-    party: "Party" = Relationship(
+    party: Optional["Party"] = Relationship(
         sa_relationship=relationship("Party", backref=backref("roles", lazy=True))
+    )
+
+
+class RoleCreate(SQLModel):
+    party_id: Optional[int] = Field(
+        None, gt=0, alias="partyId", description="ID of party"
+    )
+    name: Optional[str] = Field(None, description="Name of role")
+    max_players: Optional[int] = Field(
+        None, gt=0, alias="maxPlayers", description="Maximum number of players of role"
+    )
+
+
+class RoleUpdate(SQLModel):
+    party_id: Optional[int] = Field(
+        None, gt=0, alias="partyId", description="ID of party"
+    )
+    name: Optional[str] = Field(None, description="Name of role")
+    max_players: Optional[int] = Field(
+        None, gt=0, alias="maxPlayers", description="Maximum number of players of role"
+    )
+
+
+class RoleShort(SQLModel):
+    id: int = Field(description="ID of role")
+    party_id: Optional[int] = Field(
+        None, gt=0, alias="partyId", description="ID of party"
+    )
+    name: Optional[str] = Field(None, description="Name of role")
+    max_players: Optional[int] = Field(
+        None, gt=0, alias="maxPlayers", description="Maximum number of players of role"
     )
