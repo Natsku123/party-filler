43d29beeb1436cd10ec04272f58334c5796ded50
Max Mecklin <max@meckl.in>
2021-09-02 16:47:01 +0300

    Migrate tests to SQLModel

--- backend/test/database.py
+++ backend/test/database.py
@@ -1,8 +1,7 @@
-from fastapi import Request, HTTPException, Depends
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, Session
+from fastapi import HTTPException, Depends
+from sqlmodel import create_engine, Session, SQLModel
 
-from core.database import Base, models, crud
+from core.database import models, crud
 from test import *
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
@@ -10,15 +9,11 @@ SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
 )
-TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 
 def get_testing_get_db():
-    db = TestingSessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+    with Session(engine) as session:
+        yield session
 
 
 def get_testing_current_user(
@@ -33,22 +28,22 @@ def get_testing_current_user(
 
 
 def init_test_db():
-    Base.metadata.create_all(bind=engine)
+    SQLModel.metadata.create_all(bind=engine)
 
-    db = TestingSessionLocal()
+    with Session(engine) as db:
 
-    test_player = (
-        db.query(models.Player).filter(models.Player.discord_id == "1234567890").first()
-    )
-
-    if not test_player:
-        test_player = models.Player(
-            discord_id="1234567890", name="TEST PLAYER 1", discriminator="0001"
+        test_player = (
+            db.query(models.Player)
+            .filter(models.Player.discord_id == "1234567890")
+            .first()
         )
-        db.add(test_player)
-        db.commit()
-        db.refresh(test_player)
 
-    db.close()
+        if not test_player:
+            test_player = models.Player(
+                discord_id="1234567890", name="TEST PLAYER 1", discriminator="0001"
+            )
+            db.add(test_player)
+            db.commit()
+            db.refresh(test_player)
 
     return test_player
