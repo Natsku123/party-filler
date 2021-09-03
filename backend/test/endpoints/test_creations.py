cd2bc5337709b4131b666507e3d63adfe4619c9c
Max Mecklin <max@meckl.in>
2021-09-03 12:16:03 +0300

    Use fixtures on tests

--- backend/test/endpoints/test_creations.py
+++ backend/test/endpoints/test_creations.py
@@ -1,8 +1,9 @@
-from test.test_main import client
+import pytest
+from fastapi.testclient import TestClient
 from test import *
 
 
-def create_helper(test_channel, path):
+def create_helper(client: TestClient, test_channel, path):
     response = client.post(f"/{path}/", json=test_channel)
     assert response.status_code == 200, response.text
     data = response.json()
@@ -26,70 +27,106 @@ def create_helper(test_channel, path):
     return data
 
 
-def test_create_server_1():
-    create_helper(TEST_SERVER_1, "servers")
+@pytest.mark.usefixtures("client")
+def test_create_server_1(client: TestClient):
+    create_helper(client, TEST_SERVER_1, "servers")
 
 
-def test_create_server_2():
-    create_helper(TEST_SERVER_2, "servers")
+@pytest.mark.usefixtures("client")
+def test_create_server_2(client: TestClient):
+    create_helper(client, TEST_SERVER_2, "servers")
 
 
-def test_create_server_no_id():
+@pytest.mark.usefixtures("client")
+def test_create_server_no_id(client: TestClient):
     response = client.post("/servers/", json=TEST_SERVER_NO_ID)
     assert response.status_code == 422, response.text
 
 
-def test_create_server_no_name():
+@pytest.mark.usefixtures("client")
+def test_create_server_no_name(client: TestClient):
     response = client.post("/servers/", json=TEST_SERVER_NO_NAME)
     assert response.status_code == 422, response.text
 
 
-def create_channel_test_helper(test_channel):
-    create_helper(test_channel, "channels")
+@pytest.mark.usefixtures("client")
+def create_channel_test_helper(client: TestClient, test_channel):
+    create_helper(client, test_channel, "channels")
 
 
-def test_create_channel_1():
-    create_helper(TEST_CHANNEL_1, "channels")
+@pytest.mark.usefixtures("client")
+def test_create_channel_1(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_server_1(client)
+        test_create_server_2(client)
+    create_helper(client, TEST_CHANNEL_1, "channels")
 
 
-def test_create_channel_2():
-    create_helper(TEST_CHANNEL_2, "channels")
+@pytest.mark.usefixtures("client")
+def test_create_channel_2(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_server_1(client)
+        test_create_server_2(client)
+    create_helper(client, TEST_CHANNEL_2, "channels")
 
 
-def test_create_channel_no_name():
+@pytest.mark.usefixtures("client")
+def test_create_channel_no_name(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_server_1(client)
+        test_create_server_2(client)
     response = client.post("/channels/", json=TEST_CHANNEL_NO_NAME)
     assert response.status_code == 400, response.text
 
 
-def test_create_channel_no_id():
+@pytest.mark.usefixtures("client")
+def test_create_channel_no_id(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_server_1(client)
+        test_create_server_2(client)
     response = client.post("/channels/", json=TEST_CHANNEL_NO_ID)
     assert response.status_code == 422, response.text
 
 
-def test_create_channel_no_server():
+@pytest.mark.usefixtures("client")
+def test_create_channel_no_server(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_server_1(client)
+        test_create_server_2(client)
     response = client.post("/channels/", json=TEST_CHANNEL_NO_SERVER)
     assert response.status_code == 400, response.text
 
 
-def test_create_game_1():
-    create_helper(TEST_GAME_1, "games")
+@pytest.mark.usefixtures("client")
+def test_create_game_1(client: TestClient):
+    create_helper(client, TEST_GAME_1, "games")
 
 
-def test_create_game_2():
-    create_helper(TEST_GAME_2, "games")
+@pytest.mark.usefixtures("client")
+def test_create_game_2(client: TestClient):
+    create_helper(client, TEST_GAME_2, "games")
 
 
-def test_create_game_no_name():
+@pytest.mark.usefixtures("client")
+def test_create_game_no_name(client: TestClient):
     response = client.post("/games/", json=TEST_GAME_NO_NAME)
     assert response.status_code == 422, response.text
 
 
-def test_create_party_1():
-    party_helper(TEST_PARTY_1)
+@pytest.mark.usefixtures("client")
+def test_create_party_1(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+    party_helper(client, TEST_PARTY_1)
 
 
-def test_create_party_2():
-    party_helper(TEST_PARTY_2)
+@pytest.mark.usefixtures("client")
+def test_create_party_2(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+    party_helper(client, TEST_PARTY_2)
 
 
 def validate_test_party(test_party, data):
@@ -104,7 +141,7 @@ def validate_test_party(test_party, data):
     assert data["leader"] != "null" and data["game"] is not None
 
 
-def party_helper(test_party):
+def party_helper(client: TestClient, test_party):
     response = client.post("/parties/", json=test_party)
     assert response.status_code == 200, response.text
     data = response.json()
@@ -120,31 +157,61 @@ def party_helper(test_party):
     assert data["id"] == party_id
 
 
-def test_create_party_no_title():
+@pytest.mark.usefixtures("client")
+def test_create_party_no_title(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
     response = client.post("/parties/", json=TEST_PARTY_NO_TITLE)
     assert response.status_code == 422, response.text
 
 
-def test_create_party_no_leader():
+@pytest.mark.usefixtures("client")
+def test_create_party_no_leader(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
     response = client.post("/parties/", json=TEST_PARTY_NO_LEADER)
     assert response.status_code == 422, response.text
 
 
-def test_create_party_no_game():
+@pytest.mark.usefixtures("client")
+def test_create_party_no_game(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
     response = client.post("/parties/", json=TEST_PARTY_NO_GAME)
     assert response.status_code == 422, response.text
 
 
-def test_create_role_1():
-    create_helper(TEST_ROLE_1, "roles")
+@pytest.mark.usefixtures("client")
+def test_create_role_1(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+    create_helper(client, TEST_ROLE_1, "roles")
 
 
-def test_create_role_2():
-    create_helper(TEST_ROLE_2, "roles")
+@pytest.mark.usefixtures("client")
+def test_create_role_2(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+    create_helper(client, TEST_ROLE_2, "roles")
 
 
-def test_create_role_3():
-    create_helper(TEST_ROLE_3, "roles")
+@pytest.mark.usefixtures("client")
+def test_create_role_3(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+    create_helper(client, TEST_ROLE_3, "roles")
 
 
 def validate_member(data):
@@ -155,8 +222,8 @@ def validate_member(data):
     assert data["player"] != "null" and data["player"] is not None
 
 
-def create_member_helper(test_member, should_have_role=False):
-    post_response_data = create_helper(test_member, "members")
+def create_member_helper(client: TestClient, test_member, should_have_role=False):
+    post_response_data = create_helper(client, test_member, "members")
 
     validate_member(post_response_data)
 
@@ -180,29 +247,74 @@ def create_member_helper(test_member, should_have_role=False):
     assert id_in_list(get_party_member_response["members"], member_id)
 
 
-def test_create_member_1():
-    create_member_helper(TEST_MEMBER_1)
+@pytest.mark.usefixtures("client")
+def test_create_member_1(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
+    create_member_helper(client, TEST_MEMBER_1)
 
 
-def test_create_member_2():
-    create_member_helper(TEST_MEMBER_2)
+@pytest.mark.usefixtures("client")
+def test_create_member_2(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
+    create_member_helper(client, TEST_MEMBER_2)
 
 
-def test_create_member_3():
-    create_member_helper(TEST_MEMBER_3, True)
+@pytest.mark.usefixtures("client")
+def test_create_member_3(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
+    create_member_helper(client, TEST_MEMBER_3, True)
 
 
-def test_create_member_no_party():
+@pytest.mark.usefixtures("client")
+def test_create_member_no_party(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
     response = client.post("/members/", json=TEST_MEMBER_NO_PARTY)
     assert response.status_code == 422, response.text
 
 
-def test_create_member_no_player():
+@pytest.mark.usefixtures("client")
+def test_create_member_no_player(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
     response = client.post("/members/", json=TEST_MEMBER_NO_PLAYER)
     assert response.status_code == 422, response.text
 
 
-def delete_helper(path: str):
+def delete_helper(client: TestClient, path: str):
     response = client.get(f"/{path}/")
     assert response.status_code == 200, response.text
 
@@ -228,25 +340,66 @@ def delete_helper(path: str):
     assert len(data) == 0
 
 
-def test_delete_members():
-    delete_helper("members")
+@pytest.mark.usefixtures("client")
+def test_delete_members(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
+        test_create_member_1(client, False)
+        test_create_member_2(client, False)
+        test_create_member_3(client, False)
+    delete_helper(client, "members")
 
 
-def test_delete_roles():
-    delete_helper("roles")
+@pytest.mark.usefixtures("client")
+def test_delete_roles(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+        test_create_role_1(client, False)
+        test_create_role_2(client, False)
+        test_create_role_3(client, False)
+    delete_helper(client, "roles")
 
 
-def test_delete_parties():
-    delete_helper("parties")
+@pytest.mark.usefixtures("client")
+def test_delete_parties(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+        test_create_party_1(client, False)
+        test_create_party_2(client, False)
+    delete_helper(client, "parties")
 
 
-def test_delete_games():
-    delete_helper("games")
+@pytest.mark.usefixtures("client")
+def test_delete_games(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_game_1(client)
+        test_create_game_2(client)
+    delete_helper(client, "games")
 
 
-def test_delete_channels():
-    delete_helper("channels")
+@pytest.mark.usefixtures("client")
+def test_delete_channels(client: TestClient, create_deps=True):
+    test_create_server_1(client)
+    test_create_server_2(client)
+    if create_deps:
+        test_create_channel_1(client, False)
+        test_create_channel_2(client, False)
+    delete_helper(client, "channels")
 
 
-def test_delete_servers():
-    delete_helper("servers")
+@pytest.mark.usefixtures("client")
+def test_delete_servers(client: TestClient, create_deps=True):
+    if create_deps:
+        test_create_server_1(client)
+        test_create_server_2(client)
+    delete_helper(client, "servers")
--- backend/test/endpoints/test_multiples.py
+++ backend/test/endpoints/test_multiples.py
@@ -1,36 +1,44 @@
-from test.test_main import client
+import pytest
+from fastapi.testclient import TestClient
 
 
-def test_get_current_player():
+@pytest.mark.usefixtures("client")
+def test_get_current_player(client: TestClient):
     response = client.get("/players/")
     assert response.status_code == 200, response.text
 
 
-def test_get_servers():
+@pytest.mark.usefixtures("client")
+def test_get_servers(client: TestClient):
     response = client.get("/servers/")
     assert response.status_code == 200, response.text
 
 
-def test_get_parties():
+@pytest.mark.usefixtures("client")
+def test_get_parties(client: TestClient):
     response = client.get("/parties/")
     assert response.status_code == 200, response.text
 
 
-def test_get_members():
+@pytest.mark.usefixtures("client")
+def test_get_members(client: TestClient):
     response = client.get("/members/")
     assert response.status_code == 200, response.text
 
 
-def test_get_channels():
+@pytest.mark.usefixtures("client")
+def test_get_channels(client: TestClient):
     response = client.get("/channels/")
     assert response.status_code == 200, response.text
 
 
-def test_get_games():
+@pytest.mark.usefixtures("client")
+def test_get_games(client: TestClient):
     response = client.get("/games/")
     assert response.status_code == 200, response.text
 
 
-def test_get_roles():
+@pytest.mark.usefixtures("client")
+def test_get_roles(client: TestClient):
     response = client.get("/roles/")
     assert response.status_code == 200, response.text
